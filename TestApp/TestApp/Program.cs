182292ed0d5a6b28f95463afb0bf09b756047220
Daniil <daniilkuramov5@example.com>
2024-11-03 12:07:27 +0300

    Console add

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, People!");
+            Console.ReadKey();
+
+        }
+    }
+}
+
