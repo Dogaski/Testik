48faf6d76ee7979f413d49f518994f43c8276dd7
Daniil <daniilkuramov5@example.com>
2024-11-04 12:13:16 +0300

    init commit

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -10,7 +10,7 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, People!");
+            Console.WriteLine("Hello, Woman!");
             Console.ReadKey();
 
         }
